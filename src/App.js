e400b7a61edefa7c2cbe71cbf8f7e0fd20011228
hido02 <bestdohee@gmail.com>
2023-03-22 01:08:31 +0900

    페이지 안 보이는 오류 해결

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import useState from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from"./pages/main/MainPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,20 +12,12 @@ import './App.css';
 function App() {
   // App이 상태를 들고 있으면 됨->전송
 
-  const [posts, setPosts] = useState([
-    {id:1, title:"내용1"},
-    {id:2, title:"내용2"},
-    {id:3, title:"내용3"},
-    {id:4, title:"내용4"},
-    {id:5, title:"내용5"},
-]);
-
   return (
    <Routes>
     <Route path="/" element={<MainPage />} />
     <Route path="each/:menu" element={<EachPage/>} />
-    <Route path="/list" element={() => ListPage(posts)} />
-    <Route path="/list/write" element={() => WritePage(setPosts)} />
+    <Route path="/list" element={<ListPage />}/>
+    <Route path="/list/write" element={<WritePage />}/>
     {/*상태-writepage에서는 listpage에 접근할 수 있는 방법이 없다 */}
     </Routes>
   );
