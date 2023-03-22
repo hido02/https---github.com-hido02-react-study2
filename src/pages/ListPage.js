e12ef20a36b86eeca82a943564fa671c70d23878
hido02 <bestdohee@gmail.com>
2023-03-22 16:21:59 +0900

    글 목록, 글 쓰기 기능 추가

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,16 +1,38 @@
 import { useState } from 'react';
-import {Container, Row, Col, Button, ListGroup} from 'react-bootstrap';
+import {Container, Row, Col, Button, ListGroup, Form} from 'react-bootstrap';
 
 const ListPage = () => {
     // 서버 api에서 요청해서 데이터를 받을 것
 
+    const [post, setPost] = useState({
+        id: '',
+        title: '',
+        content: '',
+    });
+
+    const handleInputChange = (prop) => (e) => {
+        setPost({...post, [prop]: e.target.value});
+    };
+
+    // const handleInputChange2 = (e) => {
+    //     // computed property names 문법 (키값 동적할당)
+    //     setPost({
+    //         ...post, // ...을 안 쓰면 업데이트 x 기존 값 다 날라감
+    //         [e.target.name]: e.target.value,
+    //     })
+    // }
+
     const [posts, setPosts] = useState([
-        {id:1, title:"내용1"},
-        {id:2, title:"내용2"},
-        {id:3, title:"내용3"},
-        {id:4, title:"내용4"},
-        {id:5, title:"내용5"},
+        {id:1, title:"제목1", content: "내용1"},
+        {id:2, title:"제목2", content: "내용2"},
+        {id:3, title:"제목3", content: "내용3"},
+        {id:4, title:"제목4", content: "내용4"},
+        {id:5, title:"제목5", content: "내용5"},
     ]);
+
+    const handleWrite = () => {
+        setPosts([...posts, post]);
+    }
 
     return (
         <div>   
@@ -18,9 +40,23 @@ const ListPage = () => {
 <Container>
             <h1>리스트 페이지</h1>
             <hr/>
+            <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>제목</Form.Label>
+        <Form.Control type="text" placeholder="제목을 입력하세요" value={post.title} onChange={handleInputChange('title')}/>
+        <Form.Control type="text" placeholder="내용을 입력하세요" value={post.content} onChange={handleInputChange('content')} name='content'/>
+        {/* <Form.Control type="text" placeholder="내용을 입력하세요" value={post.content} onChange={handleInputChange2} name='content'/> */}
+        <Button onClick={handleWrite}>글쓰기</Button>
+      </Form.Group>
+    </Form>
+    <Row>
             <ListGroup as="ul">
-            {posts.map((post) =><Row><ListGroup.Item as="li"><Col>번호: {post.id} 제목: {post.title} </Col><Col></Col><Col><Button>삭제</Button></Col></ListGroup.Item></Row>)}
+            {posts.map((post) =><ListGroup.Item as="li"><Col>번호: {post.id} </Col>
+            <Col>제목: {post.title}</Col>
+            <Col>내용: {post.content}</Col>
+            <Button>삭제</Button></ListGroup.Item>)}
             </ListGroup>
+            </Row>
             </Container>
 
         </div>
--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -3,24 +3,14 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 const WritePage = () => {
     // write는 db에 insert
 
-    const handleWrite = () => {
-        // ListPage의 setPosts에 무엇을 담아야 함?
-        // let post = {id:6, title:"인풋값"};
-        // setPosts()해야
-    }
+
 
     return (
         <div>
 <Container>
             <h1>글쓰기 페이지</h1>
             <hr/>
-            <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>제목</Form.Label>
-        <Form.Control type="text" placeholder="제목을 입력하세요" />
-        <Button onClick={handleWrite}>글쓰기</Button>
-      </Form.Group>
-    </Form>
+            
     </Container>
 
         </div>
