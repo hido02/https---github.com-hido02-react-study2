a5bc3b4e710d5db436d1fb5c0549caa178fceff3
hido02 <bestdohee@gmail.com>
2023-03-28 15:17:24 +0900

    css를 이용한 가운데 정렬

--- src/views/AlignCenterView.js
+++ src/views/AlignCenterView.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import TextInput from '../TextInput';
 import { Button } from 'react-bootstrap';
+import '../css/AlignCenter.css';
 
 const AlignCenterView = () => {
     return (
-        <div>
-            <div class="d-flex justify-content-center">
-                <h2>가운데 정렬</h2>
-            </div>
-        </div>
+        <>
+            <div class="container">
+  <div class="child"></div>
+</div>
+</>
     );
 };
 
